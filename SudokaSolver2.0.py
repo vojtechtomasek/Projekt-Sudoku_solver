717fafd2e3119e8bc79f8f2978fbe2e4be56e80f
vojtechtomasek <113976588+vojtechtomasek@users.noreply.github.com>
2023-03-30 17:37:15 +0200

    Add files via upload

--- SudokaSolver2.0.py
+++ SudokaSolver2.0.py
@@ -15,7 +15,7 @@
    
    
 
-sudoku = [
+"""sudoku = [
     [0, 6, 0, 0, 0, 0, 0, 8, 11, 0, 0 ,15, 14, 0, 0, 16],
     [15, 11, 0, 0, 0, 16, 14, 0, 0, 0, 12, 0, 0, 6, 0, 0],
     [13, 0, 9, 12, 0, 0, 0, 0, 2, 16, 14, 0, 15, 11, 10, 0],
@@ -32,7 +32,8 @@ sudoku = [
     [0, 0, 12, 0, 1, 4, 6, 0, 16, 0, 0, 0, 11, 10, 0, 0],
     [0, 0, 5, 0, 8, 12, 13, 0, 10, 0, 0, 11, 2, 0, 0, 14],
     [3, 16, 0, 0, 10, 0, 0, 7, 0, 0, 6, 0, 0, 0, 12, 0 ]
-]
+]"""
+sudoku = []
 
 def create_sudoku(rozhrani):
     for i in range(rozhrani):
@@ -49,7 +50,9 @@ if odpoved == "1":
     for i in range(1,4):
         print(f"Napiš čísla do {i}. řádku")
         for j in range(1,4):
-            sudoku[i-1].append(int(input(f"Zadej {j}. číslo: ")))
+            x = int(input(f"Zadej {j}. číslo: "))
+            if x >= 0 and x < 10:
+                sudoku[i-1].append(x)
 
 
 elif odpoved == "2":
@@ -60,7 +63,7 @@ elif odpoved == "2":
             sudoku[i-1].append(int(input(f"Zadej {j}. číslo: ")))
 
 elif odpoved == "3":
-    create_sudoku(16)
+    #create_sudoku(16)
     for i in range(1,17):
         print(f"Napiš čísla do {i}. řádku")
         for j in range(1,17):
@@ -70,7 +73,7 @@ else:
     print("Nevibral jsi ani jednu z danych odpovedi")
 
 
-
+#/usr/local/bin/ cmd + shift + .
 def find_empty(sudo):
     #   kontrola radku
     for i in range(len(sudo)):
@@ -90,15 +93,28 @@ def valid(sudo, num, pos):
         if sudo[i][pos[1]] == num and pos[0] != i:
             return False
     
+    if odpoved == "1" or odpoved == "2":
     #   kontrola boxu
-    box_radek = pos[0] // 3
-    box_sloupec = pos[1] // 3
+        box_radek = pos[0] // 3
+        box_sloupec = pos[1] // 3
 
-    for i in range(box_radek * 3, box_radek * 3 + 3):
-        for j in range(box_sloupec * 3, box_sloupec * 3 + 3):
-            if sudo[i][j] == num and (i, j) != pos:
-                return False
-    return True
+        for i in range(box_radek * 3, box_radek * 3 + 3):
+            for j in range(box_sloupec * 3, box_sloupec * 3 + 3):
+                if sudo[i][j] == num and (i, j) != pos:
+                    return False
+        return True
+    
+    if odpoved == "3":
+        
+        box_radek = pos[0] // 4
+        box_sloupec = pos[1] // 4
+        
+        for i in range(box_radek * 4, box_radek * 4 + 4):
+            for j in range(box_sloupec * 4, box_sloupec * 4 + 4):
+                if sudo[i][j] == num and (i, j) != pos:
+                    return False
+        return True
+
 
 
 def solve(sudo):
@@ -108,16 +124,28 @@ def solve(sudo):
     else: 
         radek, sloupec = find
 
-    for i in range(1,10):
-        if valid(sudo, i, (radek, sloupec)):
-            sudo[radek][sloupec] = i
-            if solve(sudo):
-                return True
-            sudo[radek][sloupec] = 0
-    return False
+    if odpoved == "1" or odpoved == "2":
+        
+        for i in range(1,10):
+            if valid(sudo, i, (radek, sloupec)):
+                sudo[radek][sloupec] = i
+                if solve(sudo):
+                    return True
+                sudo[radek][sloupec] = 0
+        return False
+    
+    if odpoved == "3":
+        
+        for i in range(1,17):
+            if valid(sudo, i, (radek, sloupec)):
+                sudo[radek][sloupec] = i
+                if solve(sudo):
+                    return True
+                sudo[radek][sloupec] = 0
+        return False
 
 
-def print_sudoku_1(sudo):
+def print_sudoku_3x3(sudo):
     for i in range(len(sudo)):
         if i % 3 == 0 and i != 0:
             print("- - - - ")
@@ -131,7 +159,7 @@ def print_sudoku_1(sudo):
         
 
 
-def print_sudoku_2(sudo):
+def print_sudoku_9x9(sudo):
     for i in range(len(sudo)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - -")
@@ -144,23 +172,42 @@ def print_sudoku_2(sudo):
                 print(str(sudo[i][j]) + " ", end="") 
 
 
+def print_sudoku_16x16(sudo):
+    for i in range(len(sudo)):
+        if i % 4 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+        for j in range(len(sudo[0])):
+            if j % 4 == 0 and j != 0:
+                print(" | ", end="")
+            if j == 15:
+                print(sudo[i][j])
+            else:
+                print(str(sudo[i][j]) + " ", end="")
 
 
 if odpoved == "1":
     print()
-    print_sudoku_1(sudoku)
+    print_sudoku_3x3(sudoku)
     solve(sudoku)
     print("- - - - ")
-    print_sudoku_1(sudoku)
+    print_sudoku_3x3(sudoku)
 
 elif odpoved == "2":
     print()
-    print_sudoku_2(sudoku)
+    print_sudoku_9x9(sudoku)
     solve(sudoku)
     print()
     print("- - - - - - - - - - - -")
     print()
-    print_sudoku_2(sudoku)
+    print_sudoku_9x9(sudoku)
 
 
+elif odpoved == "3":
+    print()
+    print_sudoku_16x16(sudoku)
+    solve(sudoku)
+    print()
+    print("- - - - - - - - - - - - - - - - -")
+    print()
+    print_sudoku_16x16(sudoku)
     
